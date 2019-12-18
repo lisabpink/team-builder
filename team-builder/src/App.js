39f6b4af3573768191d3cdcbcab8f6d7142c20ae
LISA_COOK <lisabpink@gmail.com>
2019-12-18 17:49:01 -0500

    MVP Lisa-Cook

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+import Form from './Components/Form';
+import TeamList from './Components/TeamList';
+import data from './data';
+import { Container, Row, Col } from 'reactstrap';
+
+
 
 function App() {
+
+  const [members, setMembers] = useState(data);
+
+  const addNewPerson = person => {
+    setMembers([...members, person])
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <Container> 
+    <div>
+
+      <h1>Lisa's Team Builder</h1>
+     <Row> <TeamList teamList={members} /></Row>
+     <Col> <Form addNewPerson={addNewPerson} /></Col>
+      </div>
+      </Container>
   );
 }
-
 export default App;
---
+++ team-builder/src/data.js
@@ -0,0 +1,18 @@
+const teamData = [
+
+    {
+        id: 1, 
+        name: "Princess Kitty",
+        email: "PK@kittymeow.com",
+        role: "Full Stack Web Developer"
+    },
+
+    {
+        id: 2, 
+        name: "Meow Meow ",
+        email: "MM@cat.com",
+        role: "Data Scientist"
+    }
+]
+
+export default teamData;
